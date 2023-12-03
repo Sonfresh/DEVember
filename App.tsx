e4fd2a5289fe014de8b948202edc266ce9018466
Peterson ALEXIS <petersonalexis14@gmail.com>
2023-12-03 18:03:52 -0500

    First editing

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+
+const DAYS = [1, 2, 3, 4, 5, 6];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+
+      <FlatList
+        data = {DAYS}
+        contentContainerStyle={styles.content}
+        columnWrapperStyle={styles.column}
+        numColumns={2}
+        renderItem={({ item }) => (
+          <View style={styles.box}>
+            <Text style={styles.text}>{item}</Text>
+          </View>
+        )}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +28,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  content: {
+    gap: 10,
+    padding: 10,
+  },
+  column: {
+    gap: 10,
+  },
+  box: {
+    backgroundColor: '#F9EDE3',
+    // width: 300,
+    // height: 300,
+    flex: 1,
+    aspectRatio: 1,
+
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#9b4521',
+    borderRadius: 25,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#9b4521',
+    fontSize: 70,
+  }
 });
